55ed435da05a9de9cda9f7e27392c2aa8aab376a
Brook Pieretti <bmpieretti@gmail.com>
2021-03-21 16:05:15 -0500

    Init server and faked data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const faker = require('faker');
 
 const app = express();
 const port = 8080;
 
+const repositoryCount = 100;
+
 app.get('/api/repos', (req, res) => {
-  res.send('Hello World!');
+  const data = [];
+  for (let i = 0; i < repositoryCount; i += 1) {
+    const type = faker.helpers.shuffle(['public', 'private'])[0];
+
+    const name = faker.internet.userName();
+    data.push({
+      Id: i,
+      Url: `https://github.com/${faker.company.companyName()}/${name}`,
+      Name: name,
+      Clone_count: faker.random.number(1000),
+      Type: type,
+      Contribution_count: faker.random.number(300),
+      Commit_count: faker.random.number(10000),
+      Size: faker.random.number(100000),
+      Last_activity: faker.date.past(),
+      Risk_score: faker.random.number({ min: 1, max: 10 })
+    });
+  }
+  res.json(data);
 });
 
 app.listen(port, () => {
